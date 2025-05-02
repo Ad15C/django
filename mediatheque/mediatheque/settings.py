bc478b8a628b968939da7ad4c9a6c661b9084e2b
Ad15C <ad15canon@hotmail.com>
2025-05-02 12:50:26 +0200

    authentification_templates

--- mediatheque/mediatheque/settings.py
+++ mediatheque/mediatheque/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'mediatheque.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates', ],
+        'DIRS': [
+            BASE_DIR / 'mediatheque' / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
